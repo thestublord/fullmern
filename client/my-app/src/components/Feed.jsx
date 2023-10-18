95a488cca9efbb32241ba6d0ce523ef36a0f322c
Nicholas VanBuskirk <thestublord@gmail.com>
2023-10-18 09:36:26 -0700

    updated feed WidgetContent widgets and index.css

--- client/my-app/src/components/Feed.jsx
+++ client/my-app/src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return <div className="flex flex-col flex-1 p-2 md:flex-[0.5]">Feed</div>;
 };
 
 export default Feed;
